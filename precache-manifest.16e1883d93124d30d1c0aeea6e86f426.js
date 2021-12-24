fa95b1f539618f79b5157670f3e02757aea462e6
Rohit Singh Sagar <sagarohit@hotmail.com>
2021-12-25 03:54:21 +0530

    Updates

---
+++ precache-manifest.16e1883d93124d30d1c0aeea6e86f426.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda5ffafd3746885c663b3eba362ce86",
+    "url": "/CV-Using-React/index.html"
+  },
+  {
+    "revision": "06fffe92daca59702d3c",
+    "url": "/CV-Using-React/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "52f04a8be1e861986305",
+    "url": "/CV-Using-React/static/css/main.2964762e.chunk.css"
+  },
+  {
+    "revision": "06fffe92daca59702d3c",
+    "url": "/CV-Using-React/static/js/2.3a3c05eb.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/CV-Using-React/static/js/2.3a3c05eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f04a8be1e861986305",
+    "url": "/CV-Using-React/static/js/main.0b3da6fc.chunk.js"
+  },
+  {
+    "revision": "4e508a1062e24b674bb9",
+    "url": "/CV-Using-React/static/js/runtime-main.7219806a.js"
+  },
+  {
+    "revision": "4731361238d6b0ab3afec6e68ef5e90e",
+    "url": "/CV-Using-React/static/media/pic1.47313612.jpeg"
+  }
+]);
